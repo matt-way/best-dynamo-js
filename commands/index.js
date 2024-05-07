c75d0cb90ed9d34931ddac62f3e27e6a7086a75a
matt-way <matt@wethrift.com>
2024-05-07 13:39:14 +1000

    fixed up param cloning problem

--- commands/index.js
+++ commands/index.js
@@ -1,9 +1,11 @@
 async function* pageCommandGenerator(client, command, initialParams) {
+  let params = JSON.parse(JSON.stringify(initialParams))
   while (true) {
-    const pageResult = await client[command](initialParams)
+    const pageResult = await client[command](params)
     yield pageResult.Items
     if (pageResult.LastEvaluatedKey) {
-      initialParams.ExclusiveStartKey = pageResult.LastEvaluatedKey
+      params = JSON.parse(JSON.stringify(initialParams))
+      params.ExclusiveStartKey = pageResult.LastEvaluatedKey
     } else {
       break
     }
